544896e0ba45808ac6df024ab2f9b8f5563ad67f
leodevperez <Leobardo.Dev1208@gmail.com>
2026-07-08 16:58:28 -0600

    Git 4 login

--- hellogit4.java
+++ hellogit4.java
@@ -1,6 +1,6 @@
 public class hellogit4 {
     public static void main(String[] args) {
-        System.out.println("Hello Git4 v.4!");
+        System.out.println("Hello Git4 v login!");
     }
     
 }
